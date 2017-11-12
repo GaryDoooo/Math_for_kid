045c31da31dccd7041dd9f612e8e07400af19f53
GaryDoooo <gdu@gmx.us>
2017-11-11 22:01:22 -0600

    removed unnecessary files.

--- email_sender.py
+++ email_sender.py
@@ -4,14 +4,14 @@ import subprocess
 
 def send_list(content_list, email_addr):
     filename = "./temp.txt"
-        # subprocess.check_output(["mktemp", "/tmp/du.tmp.XXXXXX"])
+    # subprocess.check_output(["mktemp", "/tmp/du.tmp.XXXXXX"])
     # print "temp file", filename
     temp_file = open(filename, "w")
     for item in content_list:
         temp_file.write(item + "\n")
     temp_file.close()
     pipe1 = subprocess.Popen(("cat", filename), stdout=subprocess.PIPE)
-    screen_output = subprocess.check_output("sendnote", stdin=pipe1.stdout)
+    screen_output = subprocess.check_output("./sendnote", stdin=pipe1.stdout)
     print screen_output
     # print pipe1.stdout
     # call(["sendfile", filename])
